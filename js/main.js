5965b98e5e7df703e7656b82116240785e6df4e4
Your Name <you@example.com>
2015-12-02 18:53:01 +0200

    add interaction to sidebar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+/*jslint browser: true*/
+/*jslint node: true*/
+/*global $, jQuery, alert*/
+"use strict";
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        var currentY = window.pageYOffset || document.documentElement.scrollTop,
+            yLimit = 400,
+            target = $('.nav--vertical');
+        if (currentY > yLimit) {
+            target.css("opacity", "1");
+        } else {
+            target.css("opacity", "0");
+        }
+    });
+});
+
 
